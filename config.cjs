be18442139fe5764c640a170994d39765d5241b2
lium-dot <allenkangethe921@gmail.com>
2025-05-07 11:28:46 +0300

    Update config.cjs
    Signed-off-by: lium-dot <allenkangethe921@gmail.com>

--- config.cjs
+++ config.cjs
@@ -3,11 +3,11 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Buddy;;;YEQj1TxZ#0YHkQJvNtv2iDyLpJI0yeqO7za98PO_zVZ4gFLg8E4A",
+  SESSION_ID: process.env.SESSION_ID || "Buddy;;;9UggySrB#2_u0R1ngZP0yDktSKkNwWaY9xoMcmFcmC2VZ2K5CexU",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT !== undefined ? process.env.AUTO_STATUS_REACT === 'true' : true, 
-  AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
+  AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : true,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
   AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : true,
   AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
